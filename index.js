59174cfa7bd6c28f03fa9b9dcc69eab41491d0aa
khaintph16089 <khaintph16089@fpt.edu.vn>
2022-07-04 16:12:49 +0700

    alo alo

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import { Router } from 'express';
 import product from './src/routes/product.js';
+import { list } from './src/controlles/product.js';
 const app = express();
-
-
-
-// let router = module.require('./src/routes/product');
 
 // middleware
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
 
-app.use("/api", (req, res) =>{
-    app.use("api", product)
-});
+const router = Router();
+
+router.get("api/products", list)
 
 mongoose.connect("mongodb://localhost:27017/test2")
     .then(()=>{console.log("kết nỗi db thành công");})
