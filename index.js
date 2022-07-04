f128a04fefbe1b088968dfd2d68b563800861e37
khaintph16089 <khaintph16089@fpt.edu.vn>
2022-07-04 16:06:30 +0700

    alo alo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
-// import product from './src/routes/product.js';
+import product from './src/routes/product.js';
 const app = express();
 
 
